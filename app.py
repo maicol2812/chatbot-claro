f3746a75839ccd6b531afc98603117db7bf2a75c
Maicol Stiven Ocampo Vasquez <38514121@claro.com.co>
2025-06-20 15:14:22 -0500

    fix: host y puerto para Render

--- app.py
+++ app.py
@@ -91,4 +91,7 @@ def chat():
     return jsonify({"response": "❌ Algo salió mal. Intenta de nuevo."})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
